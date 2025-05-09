b6fb315b3e7a8c0805618c882250172429ea2699
ousid <imo.ossama1@gmail.com>
2025-05-09 13:59:56 +0400

    remove completion resource. WIP

--- src/Contracts/ClientContract.php
+++ src/Contracts/ClientContract.php
@@ -3,11 +3,8 @@
 namespace TogetherAI\Contracts;
 
 use TogetherAI\Contracts\Resources\ChatContract;
-use TogetherAI\Contracts\Resources\CompletionsContract;
 
 interface ClientContract
 {
-    public function completions(): CompletionsContract;
-
     public function chat(): ChatContract;
 }
